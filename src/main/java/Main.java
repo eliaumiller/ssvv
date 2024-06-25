02697bc2bb3d52aad64139c36ed7b9592b1066f3
Eli Miller <emiller@microfocus.com>
2024-06-25 22:23:55 +0300

    add vulnerability

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -108,4 +108,12 @@ public class Main {
         conn.setDoOutput(true);
         conn.setRequestProperty("Authorization", "Basic " + encoding); // Noncompliant
     }
+  public void anotherVulnerability5() throws IOException {
+        String encoding = Base64.getEncoder().encodeToString(("login:passwd").getBytes("UTF-8"));
+        URL url = new URL("www.center.com");
+        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+        conn.setRequestMethod("POST");
+        conn.setDoOutput(true);
+        conn.setRequestProperty("Authorization", "Basic " + encoding); // Noncompliant
+    }
 }
