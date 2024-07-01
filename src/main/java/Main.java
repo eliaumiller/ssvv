ed8bfa37b2a8ff5595edc3781d3472417242ea07
Eli Miller <emiller@microfocus.com>
2024-07-01 11:08:12 +0300

    sss

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -175,6 +175,22 @@ public void pleaseAddVulnerability99() {
             e.printStackTrace();
         }
     }
+public void pleaseAddVulnerability9() {
+        Connection conn = null;
+        try {
+            conn = DriverManager.getConnection("jdbc:mysql://localhost/test?" +
+                    "user=steve&password=blue"); // Sensitive
+            String uname10 = "steve";
+            String password10 = "blue";
+            conn = DriverManager.getConnection("jdbc:mysql://localhost/test?" +
+                    "user=" + uname10 + "&password=" + password10); // Sensitive
+
+            java.net.PasswordAuthentication pa = new java.net.PasswordAuthentication("userName", "1234".toCharArray());  // Sensitive
+            java.net.PasswordAuthentication p1 = new java.net.PasswordAuthentication("userName", "123478".toCharArray());  // Sensitive
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
 
     public void anotherVulnerability() throws IOException {
         String encoding = Base64.getEncoder().encodeToString(("login:passwd").getBytes("UTF-8"));
