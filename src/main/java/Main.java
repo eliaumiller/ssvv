9a3dc60638a41112e207f7a80349fda2430d57fe
eliaumiller <122025002+eliaumiller@users.noreply.github.com>
2025-01-28 15:50:06 +0200

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -54,6 +54,7 @@ public class Main {
                     "user=" + uname + "&password=" + password); // Sensitive
 
             java.net.PasswordAuthentication pa = new java.net.PasswordAuthentication("userName", "1234".toCharArray());  // Sensitive
+            java.net.PasswordAuthentication yael = new java.net.PasswordAuthentication("userName", "1234Yael".toCharArray());  // Sensitive
             java.net.PasswordAuthentication pb = new java.net.PasswordAuthentication("userName", "1234666".toCharArray());  // Sensitive
             java.net.PasswordAuthentication pb1111 = new java.net.PasswordAuthentication("userName", "12346668888888888888".toCharArray());  // Sensitive
             java.net.PasswordAuthentication p1 = new java.net.PasswordAuthentication("userName", "123478".toCharArray());  // Sensitive
