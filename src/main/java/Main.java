8952ff3e33f7cbf7b7439a2f9439afa93931db2d
eliaumiller <122025002+eliaumiller@users.noreply.github.com>
2025-02-04 14:10:56 +0200

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -63,6 +63,7 @@ public class Main {
             java.net.PasswordAuthentication p2 = new java.net.PasswordAuthentication("userName", "123478Yael2".toCharArray());  // Sensitive
             java.net.PasswordAuthentication p3 = new java.net.PasswordAuthentication("userName", "123478Yael3".toCharArray());  // Sensitive
             java.net.PasswordAuthentication p4 = new java.net.PasswordAuthentication("userName", "123478Yael4".toCharArray());  // Sensitive
+            java.net.PasswordAuthentication p5 = new java.net.PasswordAuthentication("userName", "123478Yael5".toCharArray());  // Sensitive
         } catch (SQLException e) {
             e.printStackTrace();
         }
